4a8404bd435419a47114072e850422514edd4a2d
Sam Segers <sam.sgrs@gmail.com>
2014-05-05 21:16:42 +0200

    QtCreatorResources: multiple resources per project
    Keeps track of the number of times qtcreator_add_project_resources was
    run from a project.
    Now it can be run multiple times.

--- QtCreatorResources.cmake
+++ QtCreatorResources.cmake
@@ -1,5 +1,10 @@
 # Macro to include arbitrary files in the QtCreator tree
 #  when importing cmake
 macro( qtcreator_add_project_resources resources )
-  add_custom_target( ${PROJECT_NAME}_Resources ALL SOURCES ${ARGN} )
+    if(NOT DEFINED ${PROJECT_NAME}_NO_RESOURCE_PROJECTS)
+        set(${PROJECT_NAME}_NO_RESOURCE_PROJECTS 0)
+    else(NOT DEFINED ${PROJECT_NAME}_NO_RESOURCE_PROJECTS)
+        MATH( EXPR ${PROJECT_NAME}_NO_RESOURCE_PROJECTS "${${PROJECT_NAME}_NO_RESOURCE_PROJECTS} + 1" )
+    endif(NOT DEFINED ${PROJECT_NAME}_NO_RESOURCE_PROJECTS)
+    add_custom_target( ${PROJECT_NAME}_Resources${${PROJECT_NAME}_NO_RESOURCE_PROJECTS} ALL SOURCES ${ARGN} )
 endmacro()
